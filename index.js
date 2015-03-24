e055feec5e6667eec402bdade7de4e83aa6713a7
Daniel Scholl <Daniel.Scholl@verizon.com>
2015-03-24 16:30:33 -0500

    Bug Fixed index.js to not pass pass custom response object

--- index.js
+++ index.js
@@ -13,32 +13,68 @@ var UserService = function(){
 
   self.create = function(input, done) {
     service.create(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
   self.read = function(input, done) {
     service.read(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
   self.update = function(input, done) {
     service.update(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
   self.delete = function(input, done) {
     service.delete(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
   self.list = function(input, done) {
     service.list(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
   self.search = function(input, done) {
     service.search(input, function(err, result) {
-      done(err, result);
+      if(err) {
+        done(err, null);
+      } else if(!result.success) {
+        done(result.message, null);
+      } else {
+        done(null, result.data);
+      }
     });
   };
 
